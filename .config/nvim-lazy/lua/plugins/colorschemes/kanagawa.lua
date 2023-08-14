bd75af6c94d857af92d435e0c4f25058dad79c5b
David Stadelmann <david.stadelmann@scs.ch>
2023-08-14 16:40:34 +0200

    updates of the lazy config

--- .config/nvim-lazy/lua/plugins/colorschemes/kanagawa.lua
+++ .config/nvim-lazy/lua/plugins/colorschemes/kanagawa.lua
@@ -5,27 +5,31 @@ return {
 	priority = config.get_colorscheme_priority("kanagawa"),
 	lazy = config.get_colorscheme_lazy("kanagawa"),
 	event = "VeryLazy",
-	-- opts = {
-	-- 	undercurl = true, -- enable undercurls
-	-- 	commentStyle = { italic = true },
-	-- 	functionStyle = {},
-	-- 	keywordStyle = { italic = true },
-	-- 	statementStyle = { bold = true },
-	-- 	typeStyle = {},
-	-- 	variablebuiltinStyle = { italic = true },
-	-- 	specialReturn = true, -- special highlight for the return keyword
-	-- 	specialException = true, -- special highlight for exception handling keywords
-	-- 	transparent = true, -- do not set background color
-	-- 	dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-	-- 	globalStatus = false, -- adjust window separators highlight for laststatus=3
-	-- 	terminalColors = true, -- define vim.g.terminal_color_{0,17}
-	-- 	colors = {},
-	-- 	overrides = {},
-	-- 	theme = "default", -- Load "default" theme or the experimental "light" theme
-	-- },
-	config = function()
-        if config.is_default_colorscheme("kanagawa") then
-          vim.cmd.colorscheme("kanagawa")
-        end
-    end,
+	opts = {
+		compile = false, -- enable compiling the colorscheme
+		undercurl = true, -- enable undercurls
+		commentStyle = { italic = true },
+		functionStyle = {},
+		keywordStyle = { italic = true },
+		statementStyle = { bold = true },
+		typeStyle = {},
+		transparent = false, -- do not set background color
+		dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+		terminalColors = true, -- define vim.g.terminal_color_{0,17}
+		colors = {
+			theme = {
+				all = {
+					ui = {
+						bg_gutter = "none",
+					},
+				},
+			},
+		},
+	},
+	config = function(_, opts)
+		require("kanagawa").setup(opts)
+		if config.is_default_colorscheme("kanagawa") then
+			vim.cmd.colorscheme("kanagawa")
+		end
+	end,
 }
--- .config/nvim-lazy/lua/plugins/editor/git/flog.lua
+++ .config/nvim-lazy/lua/plugins/editor/git/flog.lua
@@ -1,6 +1,9 @@
 return {
   {
     'rbong/vim-flog',
+    dependencies = {
+      "tpope/vim-fugitive"
+    },
     enabled = require('config').is_enabled("flog"),
     config = function()
       vim.cmd ([[let g:flog_default_opts = { 'max_count': 400} ]])
